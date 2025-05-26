504a579e0c7436d20f1443e06ff74eb61ece35fb
Gayatri Patil <108718904+gav1306@users.noreply.github.com>
2025-05-27 05:02:24 +0530

    units switcher added in home tabs

--- src/modules/weather/components/tabs.jsx
+++ src/modules/weather/components/tabs.jsx
@@ -1,14 +1,15 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { AllWeatherList, FavoriteWeatherList } from ".";
+import { AllWeatherList, FavoriteWeatherList, WeatherSettingsDialog } from ".";
 
 export function WeatherListTabs() {
   return (
     <Tabs defaultValue="favorites">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center gap-4 justify-between">
         <TabsList className="grid w-full md:w-[400px] grid-cols-2">
           <TabsTrigger value="favorites">Favorites</TabsTrigger>
           <TabsTrigger value="all">All</TabsTrigger>
         </TabsList>
+        <WeatherSettingsDialog />
       </div>
 
       <TabsContent value="favorites">
