c590cb65ba634f5953d7677e6e57644a3dbff938
Gayatri Patil <108718904+gav1306@users.noreply.github.com>
2025-05-27 05:04:53 +0530

    details loader UI updated

--- src/modules/weather/skeleton/details.jsx
+++ src/modules/weather/skeleton/details.jsx
@@ -7,7 +7,7 @@ export const WeatherDetailsLoading = () => {
   return (
     <section className="grid h-full grid-rows-[auto_1fr] gap-4">
       <Button asChild variant="link" className="text-lg justify-self-start">
-        <Link href="/crypto">
+        <Link href="/weather">
           <ArrowLeft /> Go Back
         </Link>
       </Button>
@@ -16,6 +16,7 @@ export const WeatherDetailsLoading = () => {
         <div className="flex justify-end items-center gap-4">
           <Skeleton className="w-[79.05px] h-[36px]" />
           <Skeleton className="w-[106.64px] h-[36px]" />
+          <Skeleton className="w-[42px] h-[36px]" />
         </div>
         <Skeleton className="h-[250px] md:h-[400px] xl:h-[500px]" />
       </div>
