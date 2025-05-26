adf1a2414723ac7a991e5496441e5b2fd70c9aaf
Gayatri Patil <gavk30@gmail.com>
2025-05-27 02:42:31 +0530

    Initial setup added
    Signed-off-by: Gayatri Patil <gavk30@gmail.com>

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/weather",
+        permanent: true,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
